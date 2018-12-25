44b4837608a266a101457b990968321a53741adc
wangfeng19930909 <1300497680@qq.com>
2018-12-25 18:28:26 +0800

    收缩textview库 commit

---
+++ library/expandtextview/src/main/java/com/hymane/expandtextview/utils/DensityUtils.java
@@ -0,0 +1,65 @@
+package com.hymane.expandtextview.utils;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * Author   :hymanme
+ * Email    :hymanme@163.com
+ * Create at 2016/3/7 0007
+ * Description:
+ */
+public class DensityUtils {
+    private DensityUtils() {
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    /**
+     * dp转px
+     *
+     * @param context 上下文
+     * @param dpVal dp值
+     * @return result in px
+     */
+    public static int dp2px(Context context, float dpVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dpVal, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * sp转px
+     *
+     * @param context 上下文
+     * @param spVal sp值
+     * @return result in px
+     */
+    public static int sp2px(Context context, float spVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
+                spVal, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * px转dp
+     *
+     * @param context 上下文
+     * @param pxVal px值
+     * @return result in dp
+     */
+    public static float px2dp(Context context, float pxVal) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (pxVal / scale);
+    }
+
+    /**
+     * px转sp
+     *
+     * @param context 上下文
+     * @param pxVal px值
+     * @return result in sp
+     */
+    public static float px2sp(Context context, float pxVal) {
+        return (pxVal / context.getResources().getDisplayMetrics().scaledDensity);
+    }
+
+}
